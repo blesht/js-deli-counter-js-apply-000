c3cafb1b2473a4f623690f95cbff5b74e4c39404
bryan lesht <bryan.lesht@colorado.edu>
2020-01-01 01:50:20 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function takeANumber(line, name){
+let placeInLine = line.length + 1;
+let output = `Welcome, ${name}. You are number ${placeInLine} in line.`;
+line.push(name);
+return output
+}
+
+
+function nowServing(line){
+  let firstPerson
+  if (line.length > 0) {
+    firstPerson = line[0]
+    line.splice(0, 1)
+    return `Currently serving ${firstPerson}.`}
+  else {
+  	return 'There is nobody waiting to be served!'
+  }
+
+}
+
+function currentLine(line){
+  let output;
+  if (line.length === 0) {
+    output = `The line is currently empty.`
+  }
+  else {
+    output = `The line is currently`
+    if (line )
+
+
